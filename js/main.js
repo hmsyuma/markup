72ea69cd7d6e8ae08295d6f6e1f922a5be1a9da0
shumashimo <shm.syuma@i.softbank.jp>
2019-05-09 09:11:51 +0900

    hogehoge

--- js/main.js
+++ js/main.js
@@ -26,3 +26,13 @@ function onPlayerReady() {
   ytPlayer.mute();// ミュートにしてから
   ytPlayer.playVideo();// 再生
 }
+
+
+$(function(){
+    $('.slider').slick({
+        infinite: true,
+        adaptiveHeight: true,
+        prevArrow: '<img src="images/new.img/arrow-right.png" class="slide-prearrow">',
+        nextArrow: '<img src="images/new.img/arrow-left.png" class="slide-nextarrow">'
+    });
+});
