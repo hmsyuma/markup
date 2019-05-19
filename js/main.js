e2c82cb02dc0e56f403d37d5841b470aa6a6538b
shumashimo <shm.syuma@i.softbank.jp>
2019-05-19 23:54:00 +0900

    case-study fixed

--- js/main.js
+++ js/main.js
@@ -3,36 +3,16 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var ytPlayer;
-
-function onYouTubeIframeAPIReady() {
-    ytPlayer = new YT.Player('player', {
-        height: 390,
-        width: 640,
-        videoId: 'iVstp5Ozw2o',
-        playerVars: {//パラメータ
-          playsinline: 1,// インライン再生指定
-          rel      : 0,// 関連動画出さない
-          controls: 0,// コントローラー出さない
-          showinfo: 0// タイトルとか動画情報出さない
-        },    
-        events: {//　イベント
-            'onReady': onPlayerReady
-        }
-    });
-}
-
 function onPlayerReady() {
   ytPlayer.mute();// ミュートにしてから
   ytPlayer.playVideo();// 再生
 }
 
-
 $(function(){
-    $('.slider').slick({
-        infinite: true,
-        adaptiveHeight: true,
-        prevArrow: '<img src="images/arrow-right.png" class="slide-prearrow">',
-        nextArrow: '<img src="images/arrow-left.png" class="slide-nextarrow">'
-    });
+  $('.slider').slick({
+    infinite: true,
+    adaptiveHeight: true,
+    nextArrow: '<img src="images/arrow-right.png" class="slide-prearrow">',
+    prevArrow: '<img src="images/arrow-left.png" class="slide-nextarrow">'
+   });
 });
