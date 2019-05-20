4b2276896f28eef84655b8e64d2213a36f7fb4c8
shumashimo <shm.syuma@i.softbank.jp>
2019-05-21 00:53:21 +0900

    レビュー修正

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-function onPlayerReady() {
-  ytPlayer.mute();// ミュートにしてから
-  ytPlayer.playVideo();// 再生
-}
-
 $(function(){
   $('.slider').slick({
     infinite: true,
