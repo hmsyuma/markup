22524dc18b406521c17ff2742d0a0b3b1ac27839
shumashimo <shm.syuma@i.softbank.jp>
2019-05-30 09:54:55 +0900

    last-fix-buddyup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-  $('.slider').slick({
+$(function() {
+  $('.slider').slick( {
     infinite: true,
     adaptiveHeight: true,
     nextArrow: '<img src="images/arrow-right.png" class="slide-prearrow">',
@@ -7,36 +7,24 @@ $(function(){
   });
 });
 
-// $(function hamburgerMenu() {
-//   $('.hamburger-button').click(function(){
-//       $('.nav-wrap').toggleClass('is-open');
-//       $('.body-wrap').toggleClass('is-overlay');
-//   });
-
-//   $('.nav-wrap').click(function(){
-//       $('.nav-wrap').removeClass('is-open');
-//       $('.body-wrap').removeClass('is-overlay');
-//   })
-// });
-
 $(function hamburgerMenu() {
-  $('.open-button').click(function(){
-    $('.nav-wrap').fadeIn();
-    $('.open-button').fadeOut(1);
-    $('.close-button').fadeIn();
-    $('.nav').fadeIn();
+  $('.open-button').click(function() {
+  $('.nav-wrap').fadeIn();
+  $('.open-button').fadeOut(1);
+  $('.close-button').fadeIn();
+  $('.nav').fadeIn();
   });
 
-  $('.close-button').click(function(){
+  $('.close-button').click(function() {
     $('.nav-wrap').fadeOut();
     $('.open-button').fadeIn();
     $('.close-button').fadeOut(1);
     $('.nav').fadeOut();
   });
 
-  $('.nav-wrap').click(function(){
-    var w = document.documentElement.scrollWidth;
-      if(w < 1080){
+  $('.nav-wrap').click(function() {
+    var width = document.documentElement.scrollWidth;
+      if(width < 1080) {
       $('.nav-wrap').fadeOut();
       $('.open-button').fadeIn();
       $('.close-button').fadeOut(1);
@@ -45,7 +33,6 @@ $(function hamburgerMenu() {
   });
 });
 
-  
 $(function stickyHeader() {
   $('.header').each(function () {
     var $window = $(window), 
@@ -54,10 +41,10 @@ $(function stickyHeader() {
 
     $window.on('scroll', function () {
       if($window.scrollTop() > headerOffsetTop) {
-        $header.addClass('sticky');
-        } else {
-        $header.removeClass('sticky');
-        }
+         $header.addClass('sticky');
+         } else {
+         $header.removeClass('sticky');
+         }
       });
   });
 });
@@ -69,7 +56,7 @@ $(function allowBtn() {
     $(this).on('click', function (event) {
           event.preventDefault();
           var w = document.documentElement.scrollWidth;
-          if(1080 <= w){
+          if(1080 <= w) {
              $el.animate({ scrollTop: 550 }, 500);
           } else if(695 < w ) {
             $el.animate({ scrollTop: 400 }, 500);  
